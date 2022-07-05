d740ca6a7862fdf0737810dcc7c4b1ee6e3fdd62
Thendo Makherana <makherana@thedigitalacademy.co.za>
2022-07-05 09:54:25 +0200

    adding functionality

--- chat/src/app/app.component.ts
+++ chat/src/app/app.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ChatClientService, ChannelService, StreamI18nService } from 'stream-chat-angular';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent  implements OnInit{
   title = 'chat';
+  constructor(
+    private chatService: ChatClientService,
+    private channelService: ChannelService,
+    private streamI18nService: StreamI18nService,
+  ) {
+    const apiKey = 'dz5f4d5kzrue';
+    const userId = 'royal-grass-8';
+    const userToken = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoicm95YWwtZ3Jhc3MtOCIsImV4cCI6MTY1NzAyMjUwM30.deV5rRSFEjIDTPKO-UF_WI64OEZahnyl9Wl686ohsYU';
+    this.chatService.init(apiKey, userId, userToken);
+    this.streamI18nService.setTranslation();
+  }
+
+  async ngOnInit() {
+    const channel = this.chatService.chatClient.channel('messaging', 'talking-about-angular', {
+      // add as many custom fields as you'd like
+      image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/2048px-Angular_full_color_logo.svg.png',
+      name: 'Talking about Angular',
+    });
+    await channel.create();
+    this.channelService.init({
+      type: 'messaging',
+      id: { $eq: 'talking-about-angular' },
+    });
+  }
 }
--- chat/src/app/app.module.ts
+++ chat/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TranslateModule } from '@ngx-translate/core';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { StreamChatModule, StreamAutocompleteTextareaModule } from 'stream-chat-angular';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule, 
+    TranslateModule.forRoot(), 
+    StreamAutocompleteTextareaModule,
+    StreamChatModule
   ],
   providers: [],
   bootstrap: [AppComponent]
