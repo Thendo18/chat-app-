dcef4e604b55eed0a0f07a09da1eae56064a7d8d
Thendo Makherana <makherana@thedigitalacademy.co.za>
2022-07-05 11:53:12 +0200

    changess

--- chat/src/app/app.module.ts
+++ chat/src/app/app.module.ts
@@ -1,27 +1,18 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-// import { TranslateModule } from '@ngx-translate/core';
 
-
-// import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-// import { StreamChatModule, StreamAutocompleteTextareaModule } from 'stream-chat-angular';
-import { MessageComponent } from './message/message.component';
-import { ChannelPreviewComponent } from './channel-preview/channel-preview.component';
-import { FormsModule } from '@angular/forms';
+import { ChatInboxComponent } from './chat-inbox/chat-inbox.component';
+
 @NgModule({
   declarations: [
     AppComponent,
-    MessageComponent,
-    ChannelPreviewComponent
+    ChatInboxComponent
   ],
   imports: [
     BrowserModule,
-    // AppRoutingModule, 
-    // TranslateModule.forRoot(), 
-    // StreamAutocompleteTextareaModule,
-    // StreamChatModule,
-    FormsModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
